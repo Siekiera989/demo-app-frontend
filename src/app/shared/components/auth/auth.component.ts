9fe468cd7dc93d714a87390eac01f2729cd1a28d
adamsiekerski <adamsiekerski@gmail.com>
2024-10-25 16:06:41 +0200

    Login page layout

--- src/app/shared/components/auth/auth.component.ts
+++ src/app/shared/components/auth/auth.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule, InputTextModule,PasswordModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
